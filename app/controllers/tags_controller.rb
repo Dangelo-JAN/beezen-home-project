0cf60c9787c94a82719134615332e6f2e856491e
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2023-03-22 17:10:37 -0400

    feature 9th: Set current user to buttons

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tags or /tags.json
   def index
