77b2a4abdb936b6bbb7cabd23536d4bd0904ea44
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2023-03-28 13:11:49 -0400

    dev 7th. commit after 1st. merge: Fix sign up bug pt.2

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_210002) do
     t.string "lastname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.bigint "role_id", null: false
+    t.bigint "role_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
