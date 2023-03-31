d5947757e0e370ee2dd1a50372063742ed1a583d
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2023-03-30 20:25:49 -0400

    Checking controller tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   load_and_authorize_resource
 
   # GET /tasks or /tasks.json
